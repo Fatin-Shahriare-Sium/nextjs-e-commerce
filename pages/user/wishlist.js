5e18cd959050e07f188f476bb97d8a48873dd380
unknown <sium1206@gmail.com>
2025-01-30 15:50:18 +0600

    alhamdulillah,fix the text-decoration probl;em

--- pages/user/wishlist.js
+++ pages/user/wishlist.js
@@ -1,35 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import useUrl from '../../component/hooks/useUrl';
-import SingleWishItem from '../../component/single-wishitem';
-import { useData } from '../../store';
+import React, { useEffect, useState } from "react";
+import useUrl from "../../component/hooks/useUrl";
+import SingleWishItem from "../../component/single-wishitem";
+import { useData } from "../../store";
 const Wishlist = () => {
-    let { auth } = useData()
-    let { url } = useUrl()
-    let [wishlist, setWishlist] = useState()
-    useEffect(() => {
-        fetch(`${url}/user/wishlist/detail?userId=${auth.user._id}`, {
-            method: 'GET'
-        }).then(res => res.json())
-            .then(data => {
-                setWishlist(data.wishlistItems)
-            })
-    }, [])
+  let { auth } = useData();
+  let { url } = useUrl();
+  let [wishlist, setWishlist] = useState();
+  useEffect(() => {
+    fetch(`${url}/user/wishlist/detail?userId=${auth.user._id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setWishlist(data.wishlistItems);
+      });
+  }, []);
 
-    let handleRemover = (id) => {
-        let exactWishlist = wishlist.filter(sig => sig._id !== id)
-        setWishlist(exactWishlist)
-        fetch(`${url}/user/wishlist?userId=${auth.user._id}&productId=${id}`, {
-            method: 'GET'
-        }).then(res => res.json())
-            .then(data => {
-
-            })
-    }
-    return (
-        <div>
-            {wishlist && wishlist.map((sig, index) => <SingleWishItem title={sig.title} img={sig.img[0].src} id={sig._id} handleRemover={handleRemover} />)}
+  let handleRemover = (id) => {
+    let exactWishlist = wishlist.filter((sig) => sig._id !== id);
+    setWishlist(exactWishlist);
+    fetch(`${url}/user/wishlist?userId=${auth.user._id}&productId=${id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {});
+  };
+  return (
+    <div>
+      {wishlist.length > 0 ? (
+        wishlist.map((sig, index) => <SingleWishItem title={sig.title} img={sig.img[0].src} id={sig._id} handleRemover={handleRemover} />)
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "80vh",
+          }}
+        >
+          <p style={{ fontSize: "2rem", fontWeight: "700" }}>You have no wishlist 😔</p>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Wishlist;
